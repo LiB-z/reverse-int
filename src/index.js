a0662402635220423e47e1dd0229e9d564603f18
LiB-z <365rik365@rambler.ru>
2022-08-06 19:15:12 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {n = n * -1;}
+    let rev = n.toString();
+    let result = "";
+      for (let i = rev.length - 1; i >= 0; i--) {
+          result = result + rev[i]
+    }
+    return result
 }
